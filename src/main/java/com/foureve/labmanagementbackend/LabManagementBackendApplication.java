3dddd615b38b3b49398e40d58bbd18b14f6d6552
GASEN1216 <GASEN1216@163.com>
2024-04-25 12:03:49 +0800

    Initial commit

---
+++ src/main/java/com/foureve/labmanagementbackend/LabManagementBackendApplication.java
@@ -0,0 +1,13 @@
+package com.foureve.labmanagementbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabManagementBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabManagementBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/foureve/labmanagementbackend/LabManagementBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foureve.labmanagementbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabManagementBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
